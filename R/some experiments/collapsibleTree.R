b983b4b419bd048382e62bcf1bcedb5f4b6f28b8
slfan2013 <slfan2013@users.noreply.github.com>
2018-02-13 23:34:45 -0800

    s

--- R/some experiments/collapsibleTree.R
+++
@@ -1,7 +0,0 @@
-devtools::install_github("AdeelK93/collapsibleTree")
-
-
-library(collapsibleTree)
-library(htmlwidgets)
-a = collapsibleTree(warpbreaks, c("wool", "tension", "breaks"))
-saveWidget(a, paste0("tree",".html"), selfcontained = FALSE)
